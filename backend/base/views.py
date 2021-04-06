602aded0c76e56a6956c59faaa10229764cd9e9c
Almighty Stranger <sufyankhan9678@gmail.com>
2021-04-06 17:13:49 +0530

    Sqlite 3 database connected and learnt about serializing the data(dictionary format)

--- backend/base/views.py
+++ backend/base/views.py
@@ -4,20 +4,24 @@ from rest_framework.decorators import api_view
 
 from rest_framework.response import Response
 
+from .models import Product
+from .serializer import ProductSerializer
 from .productsData import products
 # Create your views here.
 
 @api_view(['GET'])
 def getRoutes(request):
     return Response({"message":"high"})
+
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products=Product.objects.all()
+    serializer=ProductSerializer(products,many=True)
+    return Response(serializer.data)
+
 @api_view(['GET'])
 def getProduct(request,pk):
-    product=None
-    for i in products:
-        if i['_id']==pk:
-            product=i
-            break
-    return Response(product)
+    product=Product.objects.get(_id=pk)
+    serializer=ProductSerializer(product,many=False)
+    #serializer.data is in the form of dictionary
+    return Response(serializer.data)
