76f011ba41b4952f4e789609d422c28b9d4a04d3
Almighty Stranger <sufyankhan9678@gmail.com>
2021-04-08 23:59:22 +0530

    Updating the User Serializer

--- backend/base/serializer.py
+++ backend/base/serializer.py
@@ -8,12 +8,18 @@ class ProductSerializer(serializers.ModelSerializer):
         fields="__all__"
 class UserSerializer(serializers.ModelSerializer):
     name=serializers.SerializerMethodField(read_only=True)
+    _id=serializers.SerializerMethodField(read_only=True)
+    isAdmin=serializers.SerializerMethodField(read_only=True)
     class Meta:
         model=User
-        fields=["username","id","email","name"]
+        fields=["username","id","email","name","_id","isAdmin"]
     def get_name(self,obj):
         #first_name is comming from inbuilt user model
         name=obj.first_name
         if name=='':
             name=obj.email
         return name
+    def get__id(self,obj):
+        return obj.id
+    def get_isAdmin(self,obj):
+        return obj.is_staff
