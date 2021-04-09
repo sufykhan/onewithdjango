efd7f5524b155991215ff0e969e016f7fef5086e
Almighty Stranger <sufyankhan9678@gmail.com>
2021-04-09 23:49:46 +0530

    Updating the User Serializer to provide access and refresh token in the request.

--- backend/base/serializer.py
+++ backend/base/serializer.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from .models import Product
 from django.contrib.auth.models import User
-
+from rest_framework_simplejwt.tokens import RefreshToken
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model=Product
         fields="__all__"
+
+
 class UserSerializer(serializers.ModelSerializer):
     name=serializers.SerializerMethodField(read_only=True)
     _id=serializers.SerializerMethodField(read_only=True)
@@ -23,3 +25,12 @@ class UserSerializer(serializers.ModelSerializer):
         return obj.id
     def get_isAdmin(self,obj):
         return obj.is_staff
+
+class UserSerializerWithToken(UserSerializer):
+    token=serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model=User
+        fields=["username","id","email","name","_id","isAdmin","token"]
+    def get_token(self,obj):
+        token=RefreshToken.for_user(obj)
+        return str(token.access_token)
